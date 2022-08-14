45aef0e9bae54b3e25f0c19a3324ad0e38f40c3a
elliotmb <elliotbuckingham14@gmail.com>
2022-08-14 15:17:33 +0100

    added porfolio website card

---
+++ src/pages/Projects/images/images.ts
@@ -0,0 +1,21 @@
+import ScotlandYard from "./scotland.png";
+import PlaylistPuller from "./playlist.png";
+import RPNParser from "./rpn.jpg";
+import LeetCode from "./leetcode.png";
+import Euler from "./euler.png";
+import GE from "./matrix.png";
+import Raytracing from "./tracer.gif"
+import Polynomial from "./poly.png";
+import Portfolio from "./portfolio.png";
+
+export const images : { [key: string]: string } = {
+    scy: ScotlandYard,
+    plp: PlaylistPuller,
+    rpn: RPNParser,
+    ltc: LeetCode,
+    elr: Euler,
+    gsl: GE,
+    rtc: Raytracing,
+    ply: Polynomial,
+    ptf: Portfolio
+};
