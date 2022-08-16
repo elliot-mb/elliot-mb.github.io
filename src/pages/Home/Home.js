d7900d255907aa5ef518bd929dd1ed10c3809408
elliotmb <elliotbuckingham14@gmail.com>
2022-08-16 17:41:14 +0100

    updated cv and fixed spelling error in desc

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -21,15 +21,15 @@ export function Home() {
           </div>
         </div>
 
-        <div className="home-content"> {/*column 2*/}
+        <div className="home-content"> 
           <h2>Hello! I'm Elliot</h2>
           <div className="desc">
             <p>I'm a second-year BSc Computer Science Student at the University of Bristol, who's currently on-track for a First Class.</p>
-            <p>Aspiring software/web developer who's self-motivated and enjoys programming and solving problems. My hobbies apart from writing code include drawing, DIY/amature electronics and cycling.</p>
+            <p>Aspiring software/web developer who's self-motivated and enjoys programming and solving problems. My hobbies apart from writing code include drawing, DIY/amateur electronics and cycling.</p>
             <p>Currently looking for an internship for summer 2023!</p>
           </div>
           <h3>Contact me</h3>
-          <div className="contacts"> {/*column 1*/}
+          <div className="contacts">
             <IconButton icon={GithubIcon} url="https://github.com/elliot-mb" target="_blank" rel="noreferrer" alt="Github" text="Github" />
             <IconButton icon={EmailIcon} url="mailto:elliot.m.buckingham@gmail.com" target="_blank" rel="noreferrer" alt="Email" text="Email" />
             <IconButton icon={LinkedInIcon} url="https://www.linkedin.com/in/elliotmb/" target="_blank" rel="noreferrer" alt="LinkedIn" text="LinkedIn" />
