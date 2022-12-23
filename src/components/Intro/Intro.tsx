fb93625598512e476b0ec8e7f2cec29796f9101b
elliotmb <elliotbuckingham14@gmail.com>
2022-12-23 21:18:01 +0000

    deployed

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,4 +3,4 @@ import { ParagraphList } from "../List/List";
 
 export const Intro = <ParagraphList xs={["I'm a second-year BSc Computer Science Student at the University of Bristol, who's currently on-track for a First Class.",
 "Aspiring software/web developer who's self-motivated and enjoys programming and solving problems. My hobbies apart from writing code include drawing, DIY/amateur electronics and cycling.",
-"Currently looking for an internship for summer 2023!"]}/>;
+"Completing a 2023 summer internship!"]}/>;
