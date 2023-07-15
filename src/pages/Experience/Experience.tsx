b6591701bd9321f3b2f5f6f57eeae50cac3149a2
elliot-mb <elliotbuckingham14@gmail.com>
2023-07-15 22:16:35 +0100

    mail on req

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import CV from "../../assets/docs/CV.pdf";
+//import CV from "../../assets/docs/CV.pdf";
 import "./experience.css";
 //import Container from "../../components/Container/Container";
 
@@ -14,7 +14,8 @@ export const Experience = () => {
       <h1>Experience</h1>
       <p>My CV captures some of my important experience both in and outside of computer science.</p>
       <h2>My CV</h2>
-      <p>Take a look at <a href={CV} target="_blank" rel="noreferrer">my CV</a>.</p>
+      {/*<p>Take a look at <a href={CV} target="_blank" rel="noreferrer">my CV</a>.</p>*/}
+      <p>CV available on request, please <a href="mailto:elliot.m.buckingham@gmail.com">email me.</a></p>
     </div>
   )
 }
